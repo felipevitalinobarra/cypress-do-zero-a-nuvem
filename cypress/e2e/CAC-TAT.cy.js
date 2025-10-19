15613998c4542eccf29f86dfdc2cb6813bc0aa6f
felipevitalinobarra <felipevitalino96@gmail.com>
2025-10-19 16:59:37 -0300

    Lesson 3: Selecting options in drop-down selection fields

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -77,4 +77,22 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.contains('.success', 'Mensagem enviada com sucesso.').should('be.visible')
   })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  })
 })
