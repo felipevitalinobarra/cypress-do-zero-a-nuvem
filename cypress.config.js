1d223b1768d90f49193b8ad3612f3c4b7641aff8
felipevitalinobarra <felipevitalino96@gmail.com>
2025-10-25 16:20:41 -0300

    Lesson 11: Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "29yd36",
   viewportHeight: 880,
-  viewportWidth: 1280, 
+  viewportWidth: 1280,
   e2e: {}
 });
